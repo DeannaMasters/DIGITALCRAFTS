f805319b8e7988c1bf43d9f3ddfad226cf6e8a84
Deanna Masters <dmasters4309@gmail.com>
2021-08-24 09:17:37 -0400

    Javascript Exercises

---
+++ Week4/Day1/TrainingExercises.js
@@ -0,0 +1,26 @@
+// MADLIB
+
+function madLib(name, subject){
+  console.log(`${name}'s favorite subject in school is ${subject}`)
+}
+madLib("Deanna", "English")
+
+TIP CALCULATOR #1
+
+const good =20
+const fair =15
+const bad =10
+
+ function tipAmount(billTotal, levelofService){
+     let percentConvert = levelofService / 100;
+     let tipTotal = billTotal * percentConvert
+     console.log("Your tip is $", tipTotal)
+ }
+ tipAmount(100, fair)
+
+PRINT NUMBERS
+
+for (var input = 1; input <= 10; input++) {
+  console.log(input);
+}
+
