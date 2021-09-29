1e1a40f0f43dcca53ba3411b71f6e33f16c1067c
Deanna Masters <deanna@Deannas-MacBook-Air.local>
2021-09-29 09:31:30 -0400

    initial commit

---
+++ Week9/Day2/my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import CardContainer from './components/CardContainer'
+
+ function App() {
+  return (
+    <div className="App">
+      <h1>Pokemon App</h1>
+      <CardContainer/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ Week9/Day2/my-app/src/Components/Cards.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+export default class Cards extends Component {
+    state = {
+        flipped: false,
+    }
+
+    flipPic = () => {
+        this.setState({
+        flipped: !this.state.flipped
+    });
+    }
+
+    deletePoke = (id) => {
+        const newData = this.props.pokemon.filter(i => i.id !== id)
+        this.setState({pokemon: newData})
+
+    }
+
+    render() {
+        const { flipped } = this.state;
+        const { name, hp, sprites} = this.props.pokemon;
+        
+        return (
+            <div className="cards">
+                <img src={flipped ? sprites.back : sprites.front} />
+                <p>{name}</p>
+                <p>HP: {hp}</p>
+                <button onClick={this.flipPic}>Flip</button>
+                {/* {this.props.pokemon.map((item, id) => (<button onClick={() => this.deletePoke(id)}>Delete</button>))}    */}
+            </div>
+        )
+    }
+}
