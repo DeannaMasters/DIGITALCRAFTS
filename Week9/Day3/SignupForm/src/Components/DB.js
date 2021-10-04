3e0a8f2d3fb785f552c02858545090c276a3bacf
Deanna Masters <deanna@Deannas-MacBook-Air.local>
2021-10-04 00:16:09 -0400

    Signup

---
+++ Week9/Day3/SignupForm/src/Components/DB.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DB.css'
+
+function DB(props) {
+    const list = props
+    
+    return (
+        <div className="DatabaseDiv">
+            <h1>Database</h1>
+            <div className="Names">
+            <ul>
+                {list.credentials.map((items)=> ( <li>{items}</li>))}
+            </ul>
+            </div>
+
+        </div>
+    )
+}
+export default DB
